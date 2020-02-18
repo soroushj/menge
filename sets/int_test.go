ffb3176bef991a20ba76e3cf46cba9515a17db9e
Soroush Javadi <soroush.javadi@gmail.com>
2020-02-19 03:05:55 +0330

    Improve test

--- sets/int_test.go
+++ sets/int_test.go
@@ -3,61 +3,71 @@ package sets
 import "testing"
 
 func TestIntSet(t *testing.T) {
+	// NewIntSet() -> {}
 	s := NewIntSet()
 	if s.Has(1) {
-		t.Error("Newly-created set has an element (1)")
+		t.Error("NewIntSet() Has(1) got: true - want: false - expected set: {}")
 	}
 	if z := s.Size(); z != 0 {
-		t.Errorf("Expected size to be 0, but got %v", z)
+		t.Errorf("NewIntSet() Size() got: %v - want: 0 - expected set: {}", z)
 	}
 	if !s.IsEmpty() {
-		t.Error("IsEmpty() returned false for a newly-created set")
+		t.Error("NewIntSet() IsEmpty() got: false - want: true - expected set: {}")
 	}
 	if a := s.AsSlice(); len(a) != 0 {
-		t.Errorf("AsSlice() returned %v for an empty set", a)
+		t.Errorf("NewIntSet() AsSlice() got: %v - want: [] - expected set: {}", a)
 	}
 	if r := s.String(); r != "{}" {
-		t.Errorf("String() returned %v for an empty set", r)
+		t.Errorf("NewIntSet() String() got: %v - want: {} - expected set: {}", r)
 	}
+	// NewIntSet() Add(1, 2, 1) -> {1 2}
 	s.Add(1, 2, 1)
 	if !s.Has(1) {
-		t.Error("Set doesn't have an added element (1)")
+		t.Error("NewIntSet() Add(1, 2, 1) Has(1) got: false - want: true - expected set: {1 2}")
 	}
 	if !s.Has(2) {
-		t.Error("Set doesn't have an added element (2)")
+		t.Error("NewIntSet() Add(1, 2, 1) Has(2) got: false - want: true - expected set: {1 2}")
 	}
 	if z := s.Size(); z != 2 {
-		t.Errorf("Expected size to be 2, but got %v", z)
+		t.Errorf("NewIntSet() Add(1, 2, 1) Size() got: %v - want: 2 - expected set: {1 2}", z)
 	}
 	if s.IsEmpty() {
-		t.Error("IsEmpty() returned true for a non-empty set")
+		t.Error("NewIntSet() Add(1, 2, 1) IsEmpty() got: true - want: false - expected set: {1 2}")
 	}
 	if a := s.AsSlice(); len(a) != 2 || !((a[0] == 1 && a[1] == 2) || (a[0] == 2 && a[1] == 1)) {
-		t.Errorf("AsSlice() returned %v for set {1 2}", a)
+		t.Errorf("NewIntSet() Add(1, 2, 1) AsSlice() got: %v - want: [1 2] or [2 1] - expected set: {1 2}", a)
 	}
 	if r := s.String(); r != "{1 2}" && r != "{2 1}" {
-		t.Errorf("String() returned %v for set {1 2}", r)
+		t.Errorf("NewIntSet() Add(1, 2, 1) String() got: %v - want: {1 2} or {2 1} - expected set: {1 2}", r)
 	}
-	s.Remove(1, 2, 2)
-	if s.Has(1) {
-		t.Error("Set has a removed element (1)")
+	// NewIntSet() Add(1, 2, 1) Remove(2, 2) -> {1}
+	s.Remove(2, 2)
+	if !s.Has(1) {
+		t.Error("NewIntSet() Add(1, 2, 1) Remove(2, 2) Has(1) got: false - want: true - expected set: {1}")
 	}
 	if s.Has(2) {
-		t.Error("Set has a removed element (2)")
+		t.Error("NewIntSet() Add(1, 2, 1) Remove(2, 2) Has(2) got: true - want: false - expected set: {1}")
 	}
-	if z := s.Size(); z != 0 {
-		t.Errorf("Expected size to be 0, but got %v", z)
+	if z := s.Size(); z != 1 {
+		t.Errorf("NewIntSet() Add(1, 2, 1) Remove(2, 2) Size() got: %v - want: 1 - expected set: {1}", z)
 	}
-	if !s.IsEmpty() {
-		t.Error("IsEmpty() returned false for an empty set")
+	if s.IsEmpty() {
+		t.Error("NewIntSet() Add(1, 2, 1) Remove(2, 2) IsEmpty() got: true - want: false - expected set: {1}")
 	}
+	if a := s.AsSlice(); len(a) != 1 || a[0] != 1 {
+		t.Errorf("NewIntSet() Add(1, 2, 1) Remove(2, 2) AsSlice() got: %v - want: [1] - expected set: {1}", a)
+	}
+	if r := s.String(); r != "{1}" {
+		t.Errorf("NewIntSet() Add(1, 2, 1) Remove(2, 2) String() got: %v - want: {1} - expected set: {1}", r)
+	}
+	// NewIntSet() Add(1, 2, 1) Remove(2, 2) Clear() -> {}
 	s.Clear()
 	if !s.IsEmpty() {
-		t.Error("Cleared an empty set, but no longer is empty")
+		t.Error("NewIntSet() Add(1, 2, 1) Remove(2, 2) Clear() IsEmpty() got: false - want: true - expected set: {}")
 	}
-	s.Add(1, 2, 3)
+	// NewIntSet() Add(1, 2, 1) Remove(2, 2) Clear() Clear() -> {}
 	s.Clear()
 	if !s.IsEmpty() {
-		t.Error("Cleared a set, but is not empty")
+		t.Error("NewIntSet() Add(1, 2, 1) Remove(2, 2) Clear() Clear() IsEmpty() got: false - want: true - expected set: {}")
 	}
 }
