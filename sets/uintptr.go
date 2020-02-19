95daed52fbb1da3133f031c98a1716ff5bc526ce
Soroush Javadi <soroush.javadi@gmail.com>
2020-02-19 17:03:37 +0330

    Add uintptr

---
+++ sets/uintptr.go
@@ -0,0 +1,163 @@
+package sets
+
+import (
+	"fmt"
+	"strings"
+)
+
+// UIntPtrSet represents a set of uintptr elements
+type UIntPtrSet map[uintptr]struct{}
+
+// Add adds zero or more elements to the set
+func (s UIntPtrSet) Add(elems ...uintptr) {
+	for _, e := range elems {
+		s[e] = struct{}{}
+	}
+}
+
+// Remove removes zero or more elements from the set
+func (s UIntPtrSet) Remove(elems ...uintptr) {
+	for _, e := range elems {
+		delete(s, e)
+	}
+}
+
+// Empty empties the set
+func (s UIntPtrSet) Empty() {
+	for e := range s {
+		delete(s, e)
+	}
+}
+
+// Has indicates whether the set has an element
+func (s UIntPtrSet) Has(elem uintptr) bool {
+	_, ok := s[elem]
+	return ok
+}
+
+// Size returns the size of the set
+func (s UIntPtrSet) Size() int {
+	return len(s)
+}
+
+// IsEmpty indicates whether the set is empty
+func (s UIntPtrSet) IsEmpty() bool {
+	return len(s) == 0
+}
+
+// AsSlice returns an equivalent slice with no specific order of the elements
+func (s UIntPtrSet) AsSlice() []uintptr {
+	a := make([]uintptr, len(s))
+	i := 0
+	for e := range s {
+		a[i] = e
+		i++
+	}
+	return a
+}
+
+// String returns a string representation of the set
+func (s UIntPtrSet) String() string {
+	b := &strings.Builder{}
+	b.Grow(len(s) * 4)
+	fmt.Fprint(b, "{")
+	first := true
+	for e := range s {
+		if first {
+			first = false
+			fmt.Fprintf(b, "%v", e)
+		} else {
+			fmt.Fprintf(b, " %v", e)
+		}
+	}
+	fmt.Fprint(b, "}")
+	return b.String()
+}
+
+// Equals indicates whether s and t are equal
+func (s UIntPtrSet) Equals(t UIntPtrSet) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	r := make(UIntPtrSet, len(s))
+	for e := range s {
+		r[e] = struct{}{}
+	}
+	for e := range t {
+		delete(r, e)
+	}
+	return len(r) == 0
+}
+
+// Union returns the union of s and t
+func (s UIntPtrSet) Union(t UIntPtrSet) UIntPtrSet {
+	r := make(UIntPtrSet, len(s)+len(t))
+	for e := range s {
+		r[e] = struct{}{}
+	}
+	for e := range t {
+		r[e] = struct{}{}
+	}
+	return r
+}
+
+// Intersection returns the intersection of s and t
+func (s UIntPtrSet) Intersection(t UIntPtrSet) UIntPtrSet {
+	var small, large UIntPtrSet
+	if len(s) <= len(t) {
+		small, large = s, t
+	} else {
+		small, large = t, s
+	}
+	r := make(UIntPtrSet, len(small))
+	for e := range small {
+		if large.Has(e) {
+			r[e] = struct{}{}
+		}
+	}
+	return r
+}
+
+// Difference returns the difference of s and t, i.e., s - t
+func (s UIntPtrSet) Difference(t UIntPtrSet) UIntPtrSet {
+	r := make(UIntPtrSet, len(s))
+	for e := range s {
+		if !t.Has(e) {
+			r[e] = struct{}{}
+		}
+	}
+	return r
+}
+
+// IsSubsetOf indicates whether s is a subset of t
+func (s UIntPtrSet) IsSubsetOf(t UIntPtrSet) bool {
+	for e := range s {
+		if !t.Has(e) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsDisjointFrom indicates whether s and t are disjoint
+func (s UIntPtrSet) IsDisjointFrom(t UIntPtrSet) bool {
+	var small, large UIntPtrSet
+	if len(s) <= len(t) {
+		small, large = s, t
+	} else {
+		small, large = t, s
+	}
+	for e := range small {
+		if large.Has(e) {
+			return false
+		}
+	}
+	return true
+}
+
+// NewUIntPtrSet returns a new UIntPtrSet containing zero or more elements
+func NewUIntPtrSet(elems ...uintptr) UIntPtrSet {
+	s := make(UIntPtrSet, len(elems))
+	s.Add(elems...)
+	return s
+}
---
+++ sets/uintptr_test.go
@@ -0,0 +1,204 @@
+package sets
+
+import "testing"
+
+func TestUIntPtrSetBasics(t *testing.T) {
+	// NewUIntPtrSet() -> {}
+	s := NewUIntPtrSet()
+	if s.Has(1) {
+		t.Error("NewUIntPtrSet() Has(1) got: true - want: false - expected set: {}")
+	}
+	if z := s.Size(); z != 0 {
+		t.Errorf("NewUIntPtrSet() Size() got: %v - want: 0 - expected set: {}", z)
+	}
+	if !s.IsEmpty() {
+		t.Error("NewUIntPtrSet() IsEmpty() got: false - want: true - expected set: {}")
+	}
+	if a := s.AsSlice(); len(a) != 0 {
+		t.Errorf("NewUIntPtrSet() AsSlice() got: %v - want: [] - expected set: {}", a)
+	}
+	if r := s.String(); r != "{}" {
+		t.Errorf("NewUIntPtrSet() String() got: %v - want: {} - expected set: {}", r)
+	}
+	// NewUIntPtrSet() Add(1, 2, 1) -> {1 2}
+	s.Add(1, 2, 1)
+	if !s.Has(1) {
+		t.Error("NewUIntPtrSet() Add(1, 2, 1) Has(1) got: false - want: true - expected set: {1 2}")
+	}
+	if !s.Has(2) {
+		t.Error("NewUIntPtrSet() Add(1, 2, 1) Has(2) got: false - want: true - expected set: {1 2}")
+	}
+	if z := s.Size(); z != 2 {
+		t.Errorf("NewUIntPtrSet() Add(1, 2, 1) Size() got: %v - want: 2 - expected set: {1 2}", z)
+	}
+	if s.IsEmpty() {
+		t.Error("NewUIntPtrSet() Add(1, 2, 1) IsEmpty() got: true - want: false - expected set: {1 2}")
+	}
+	if a := s.AsSlice(); len(a) != 2 || !((a[0] == 1 && a[1] == 2) || (a[0] == 2 && a[1] == 1)) {
+		t.Errorf("NewUIntPtrSet() Add(1, 2, 1) AsSlice() got: %v - want: [1 2] or [2 1] - expected set: {1 2}", a)
+	}
+	if r := s.String(); r != "{1 2}" && r != "{2 1}" {
+		t.Errorf("NewUIntPtrSet() Add(1, 2, 1) String() got: %v - want: {1 2} or {2 1} - expected set: {1 2}", r)
+	}
+	// NewUIntPtrSet() Add(1, 2, 1) Remove(2, 2) -> {1}
+	s.Remove(2, 2)
+	if !s.Has(1) {
+		t.Error("NewUIntPtrSet() Add(1, 2, 1) Remove(2, 2) Has(1) got: false - want: true - expected set: {1}")
+	}
+	if s.Has(2) {
+		t.Error("NewUIntPtrSet() Add(1, 2, 1) Remove(2, 2) Has(2) got: true - want: false - expected set: {1}")
+	}
+	if z := s.Size(); z != 1 {
+		t.Errorf("NewUIntPtrSet() Add(1, 2, 1) Remove(2, 2) Size() got: %v - want: 1 - expected set: {1}", z)
+	}
+	if s.IsEmpty() {
+		t.Error("NewUIntPtrSet() Add(1, 2, 1) Remove(2, 2) IsEmpty() got: true - want: false - expected set: {1}")
+	}
+	if a := s.AsSlice(); len(a) != 1 || a[0] != 1 {
+		t.Errorf("NewUIntPtrSet() Add(1, 2, 1) Remove(2, 2) AsSlice() got: %v - want: [1] - expected set: {1}", a)
+	}
+	if r := s.String(); r != "{1}" {
+		t.Errorf("NewUIntPtrSet() Add(1, 2, 1) Remove(2, 2) String() got: %v - want: {1} - expected set: {1}", r)
+	}
+	// NewUIntPtrSet(2, 1, 2) -> {1 2}
+	s = NewUIntPtrSet(2, 1, 2)
+	if !s.Has(1) {
+		t.Error("NewUIntPtrSet(2, 1, 2) Has(1) got: false - want: true - expected set: {1 2}")
+	}
+	if !s.Has(2) {
+		t.Error("NewUIntPtrSet(2, 1, 2) Has(2) got: false - want: true - expected set: {1 2}")
+	}
+	if z := s.Size(); z != 2 {
+		t.Errorf("NewUIntPtrSet(2, 1, 2) Size() got: %v - want: 2 - expected set: {1 2}", z)
+	}
+	if s.IsEmpty() {
+		t.Error("NewUIntPtrSet(2, 1, 2) IsEmpty() got: true - want: false - expected set: {1 2}")
+	}
+	if a := s.AsSlice(); len(a) != 2 || !((a[0] == 1 && a[1] == 2) || (a[0] == 2 && a[1] == 1)) {
+		t.Errorf("NewUIntPtrSet(2, 1, 2) AsSlice() got: %v - want: [1 2] or [2 1] - expected set: {1 2}", a)
+	}
+	if r := s.String(); r != "{1 2}" && r != "{2 1}" {
+		t.Errorf("NewUIntPtrSet(2, 1, 2) String() got: %v - want: {1 2} or {2 1} - expected set: {1 2}", r)
+	}
+	// NewUIntPtrSet(2, 1, 2) Empty() -> {}
+	s.Empty()
+	if !s.IsEmpty() {
+		t.Error("NewUIntPtrSet(2, 1, 2) Empty() IsEmpty() got: false - want: true - expected set: {}")
+	}
+	// NewUIntPtrSet(2, 1, 2) Empty() Empty() -> {}
+	s.Empty()
+	if !s.IsEmpty() {
+		t.Error("NewUIntPtrSet(2, 1, 2) Empty() Empty() IsEmpty() got: false - want: true - expected set: {}")
+	}
+}
+
+func TestUIntPtrSetEquals(t *testing.T) {
+	a := NewUIntPtrSet()
+	b := NewUIntPtrSet()
+	if !a.Equals(b) {
+		t.Errorf("%v.Equals(%v) got: false - want: true", a, b)
+	}
+	a = NewUIntPtrSet(1, 2)
+	b = NewUIntPtrSet(1, 2)
+	if !a.Equals(b) {
+		t.Errorf("%v.Equals(%v) got: false - want: true", a, b)
+	}
+	a = NewUIntPtrSet(1, 2)
+	b = NewUIntPtrSet(2)
+	if a.Equals(b) {
+		t.Errorf("%v.Equals(%v) got: true - want: false", a, b)
+	}
+	a = NewUIntPtrSet(1)
+	b = NewUIntPtrSet(2)
+	if a.Equals(b) {
+		t.Errorf("%v.Equals(%v) got: true - want: false", a, b)
+	}
+}
+
+func TestUIntPtrSetUnion(t *testing.T) {
+	a := NewUIntPtrSet()
+	b := NewUIntPtrSet()
+	w := NewUIntPtrSet()
+	if g := a.Union(b); !g.Equals(w) {
+		t.Errorf("%v.Union(%v) got: %v - want: %v", a, b, g, w)
+	}
+	a = NewUIntPtrSet(1, 2)
+	b = NewUIntPtrSet(2, 3)
+	w = NewUIntPtrSet(1, 2, 3)
+	if g := a.Union(b); !g.Equals(w) {
+		t.Errorf("%v.Union(%v) got: %v - want: %v", a, b, g, w)
+	}
+}
+
+func TestUIntPtrSetIntersection(t *testing.T) {
+	a := NewUIntPtrSet()
+	b := NewUIntPtrSet()
+	w := NewUIntPtrSet()
+	if g := a.Intersection(b); !g.Equals(w) {
+		t.Errorf("%v.Intersection(%v) got: %v - want: %v", a, b, g, w)
+	}
+	a = NewUIntPtrSet(1, 2, 3)
+	b = NewUIntPtrSet(3, 4)
+	w = NewUIntPtrSet(3)
+	if g := a.Intersection(b); !g.Equals(w) {
+		t.Errorf("%v.Intersection(%v) got: %v - want: %v", a, b, g, w)
+	}
+	if g := b.Intersection(a); !g.Equals(w) {
+		t.Errorf("%v.Intersection(%v) got: %v - want: %v", b, a, g, w)
+	}
+}
+
+func TestUIntPtrSetDifference(t *testing.T) {
+	a := NewUIntPtrSet()
+	b := NewUIntPtrSet()
+	w := NewUIntPtrSet()
+	if g := a.Difference(b); !g.Equals(w) {
+		t.Errorf("%v.Difference(%v) got: %v - want: %v", a, b, g, w)
+	}
+	a = NewUIntPtrSet(1, 2)
+	b = NewUIntPtrSet(2, 3)
+	w = NewUIntPtrSet(1)
+	if g := a.Difference(b); !g.Equals(w) {
+		t.Errorf("%v.Difference(%v) got: %v - want: %v", a, b, g, w)
+	}
+}
+
+func TestUIntPtrSetIsSubsetOf(t *testing.T) {
+	a := NewUIntPtrSet()
+	b := NewUIntPtrSet()
+	if !a.IsSubsetOf(b) {
+		t.Errorf("%v.IsSubsetOf(%v) got: false - want: true", a, b)
+	}
+	a = NewUIntPtrSet(1, 2)
+	b = NewUIntPtrSet(1, 2, 3)
+	if !a.IsSubsetOf(b) {
+		t.Errorf("%v.IsSubsetOf(%v) got: false - want: true", a, b)
+	}
+	if b.IsSubsetOf(a) {
+		t.Errorf("%v.IsSubsetOf(%v) got: true - want: false", b, a)
+	}
+}
+
+func TestUIntPtrSetIsDisjointFrom(t *testing.T) {
+	a := NewUIntPtrSet()
+	b := NewUIntPtrSet()
+	if !a.IsDisjointFrom(b) {
+		t.Errorf("%v.IsDisjointFrom(%v) got: false - want: true", a, b)
+	}
+	a = NewUIntPtrSet(1, 2)
+	b = NewUIntPtrSet(3, 4, 5)
+	if !a.IsDisjointFrom(b) {
+		t.Errorf("%v.IsDisjointFrom(%v) got: false - want: true", a, b)
+	}
+	if !b.IsDisjointFrom(a) {
+		t.Errorf("%v.IsDisjointFrom(%v) got: false - want: true", b, a)
+	}
+	a = NewUIntPtrSet(1, 2)
+	b = NewUIntPtrSet(2, 3, 4)
+	if a.IsDisjointFrom(b) {
+		t.Errorf("%v.IsDisjointFrom(%v) got: true - want: false", a, b)
+	}
+	if b.IsDisjointFrom(a) {
+		t.Errorf("%v.IsDisjointFrom(%v) got: true - want: false", b, a)
+	}
+}
